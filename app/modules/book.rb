74122823280471e89473bce6e160c6584f8bcc33
Mendel Stillerman <menstiler@gmail.com>
2019-05-28 12:01:04 -0400

    testing associations

--- app/modules/book.rb
+++ app/modules/book.rb
@@ -1,2 +1,4 @@
 class Book < ActiveRecord::Base
+  has_many :checkout
+  has_many :users, through: :checkout
 end
--- app/modules/user.rb
+++ app/modules/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  has_many :checkouts
+  has_many :books, through: :checkout
 end
