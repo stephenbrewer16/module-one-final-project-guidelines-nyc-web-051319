f1828c4af895ddcc0fe62303e57b6a1d72955e7a
Mendel Stillerman <menstiler@gmail.com>
2019-05-28 17:11:20 -0400

    added search functionality

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,7 @@
 require_relative '../config/environment'
 require 'rest-client'
 require 'JSON'
+require 'pry'
 
 def run
   welcome
@@ -14,21 +15,54 @@ def welcome
   create_or_find_user(name)
   puts "Hello #{name}!"
   puts "What would you like to do?"
+  task = gets.chomp
+  action(task)
+end
+
+def action(input)
+  # case input
+  if input == "1"
+    puts "What book would you like to checkout?"
+      book_query = gets.chomp
+    search_for_book(book_query)
+  end
 end
 
 def create_or_find_user(name)
   User.find_or_create_by(name: name)
 end
 
-def search_for_book
-  input = gets.chomp
-  query = RestClient.get("https://www.googleapis.com/books/v1/volumes?q=#{input}")
+# def most_popular
+#   Book.all.max
+# end
+def find_book_in_api(book)
+  query = RestClient.get("https://www.googleapis.com/books/v1/volumes?q=#{book}")
   result = JSON.parse(query)
+  if result["totalItems"] == 0
+    puts "Sorry :( We have no record of this book. Please try again..."
+  else
+    book_hash = grab_book_info(result)
+    create_book(book_hash)
+    # show_book(book)
+  end
 end
 
-def grab_book_info
-  data = search_for_book
-  books = data["items"]
+def search_for_book(book)
+  if Book.find_by(title: book) == nil
+    find_book_in_api(book)
+  else Book.find_by(title: book)
+    show_book(book)
+  end
+end
+
+def show_book(book)
+  Book.find_by(title: book)
+  # puts " "
+end
+
+def grab_book_info(book)
+  # binding.pry
+  books = book["items"]
   book_hash = {}
   books.find do |book|
     book_hash[:title] = book["volumeInfo"]["title"]
@@ -39,11 +73,15 @@ def grab_book_info
   book_hash
 end
 
-def create_book
-  book_hash = grab_book_info
+def create_book(book_hash)
   Book.create(book_hash)
 end
 
+def checkout(user, book)
+  Checkout.create(user_id: user.id, book_id: book.id)
+end
 
 run
+# puts most_popuar
+# add_to_wishlist("mendel", "Harry Potter and the Cursed Child – Parts One and Two (Special Rehearsal Edition)")
 # puts "HELLO WORLD"
