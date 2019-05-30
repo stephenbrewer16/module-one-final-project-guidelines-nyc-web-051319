a0c82bd59a428dc0273fae87ef35d3a6061a976b
Mendel Stillerman <menstiler@gmail.com>
2019-05-30 15:44:16 -0400

    added bookworm image

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,19 +3,4 @@ class Book < ActiveRecord::Base
   has_many :users, through: :checkouts
 
 
-  def self.most_popular
-    self.all.max_by do |book|
-      book.users.count
-    end
-    # binding.pry
-  end
-
-  def self.random_book
-    self.all.sample
-  end
-
-  # find the longest book based on page_count
-
-  # read book description (would have to add book description as column)
-
 end
